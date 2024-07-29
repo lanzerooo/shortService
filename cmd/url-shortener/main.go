0b3a86cb2228c8a924ba277c101a74e0f1e74a43
lanzerooo <tifjt50@gmail.com>
2024-07-30 01:15:34 +0300

    1

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"http-app/internal/config"
+)
+
+func main() {
+	
+	//TODO: init config: cleanenv
+	cfg := config.MustLoad()
+	
+	fmt.Println(cfg)
+
+	//TODO: init logger: slog
+
+	//TODO: init storage: sqlite
+
+	//TODO: init router: chi, render
+
+	//TODO: run server
+}
