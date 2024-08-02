ce2d2a90b20cdb0806ce0a162704834a2e383942
lanzerooo <tifjt50@gmail.com>
2024-08-02 22:31:38 +0300

    database

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"http-app/internal/config"
+	"http-app/internal/lib/logger/sl"
+	"http-app/internal/storage/sqlite"
 	"log/slog"
 	"os"
 )
 
-const(
+const (
 	envLocal = "local"
-	envDev = "dev"
-	envProd = "prod"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -29,14 +31,21 @@ func main() {
 
 	//TODO: init storage: sqlite
 
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+
 	//TODO: init router: chi, render
 
 	//TODO: run server
 }
 
-
-func setupLogger(env string) *slog.Logger{
-var log *slog.Logger
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
 	switch env {
 	case envLocal:
 		log = slog.New(
